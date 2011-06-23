548e1f1e0e75779c09b4f654d6b68b664ab8d4fa
llarreta <llarreta@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-06-23 20:36:47 +0000

    Mejoras en estructura general y separacion de dependencias de jars

--- iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/backend/services/TestService.java
+++ iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/backend/services/TestService.java
@@ -1,12 +1,19 @@
 package ar.com.AmberSoft.iEvenTask.backend.services;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import ar.com.AmberSoft.iEvenTask.backend.entities.Operation;
 import ar.com.AmberSoft.iEvenTask.server.hibernate.HibernateUtil;
 
 public class TestService {
 	
 	
 	public void conectarse(){
-		HibernateUtil.getSessionFactory();
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		session.save(new Operation());
+		transaction.commit();
 	}
 	
 	
