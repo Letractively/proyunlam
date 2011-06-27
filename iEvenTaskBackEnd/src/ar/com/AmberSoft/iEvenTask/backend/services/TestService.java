e4ad623b403102811bcd7e7aaeecf4d0031d08f7
llarreta <llarreta@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-06-27 19:30:18 +0000

    Se comienzan a mapear las entidades reales con las tablas de la BD

--- iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/backend/services/TestService.java
+++ iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/backend/services/TestService.java
@@ -3,18 +3,20 @@ package ar.com.AmberSoft.iEvenTask.backend.services;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-import ar.com.AmberSoft.iEvenTask.backend.entities.Operation;
+import ar.com.AmberSoft.iEvenTask.backend.entities.Perfil;
 import ar.com.AmberSoft.iEvenTask.server.hibernate.HibernateUtil;
 
 public class TestService {
-
-	public void conectarse() {
+	
+	
+	public void conectarse(){
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		Transaction transaction = session.beginTransaction();
-		Operation operation = new Operation();
+		Perfil operation = new Perfil();
 		operation.setId("prueba");
 		session.save(operation);
 		transaction.commit();
 	}
-
+	
+	
 }
