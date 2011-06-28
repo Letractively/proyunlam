69f3d5a45d8061d8e6e145ca5885261de56c39a8
llarreta <llarreta@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-06-28 19:22:10 +0000

    Ejemplos de validaciones en un formulario

--- trunk/iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/client/ProfilesWindow.java
+++ trunk/iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/client/ProfilesWindow.java
@@ -1,20 +1,28 @@
 package ar.com.AmberSoft.iEvenTask.client;
 
-import com.extjs.gxt.ui.client.Style.Orientation;
-import com.extjs.gxt.ui.client.widget.Window;
-import com.extjs.gxt.ui.client.widget.layout.RowLayout;
-import com.extjs.gxt.ui.client.widget.TabPanel;
-import com.extjs.gxt.ui.client.widget.TabItem;
-import com.extjs.gxt.ui.client.widget.layout.RowData;
+import ar.com.AmberSoft.iEvenTask.client.validaciones.ValidaObligatorio;
+
 import com.extjs.gxt.ui.client.Style;
+import com.extjs.gxt.ui.client.Style.Orientation;
+import com.extjs.gxt.ui.client.event.ButtonEvent;
+import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.util.Margins;
-import com.extjs.gxt.ui.client.widget.button.ButtonBar;
-import com.extjs.gxt.ui.client.widget.button.IconButton;
-import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.HorizontalPanel;
+import com.extjs.gxt.ui.client.widget.Info;
+import com.extjs.gxt.ui.client.widget.TabItem;
+import com.extjs.gxt.ui.client.widget.TabPanel;
 import com.extjs.gxt.ui.client.widget.VerticalPanel;
-import com.extjs.gxt.ui.client.widget.Text;
+import com.extjs.gxt.ui.client.widget.Window;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.form.Field;
+import com.extjs.gxt.ui.client.widget.form.LabelField;
 import com.extjs.gxt.ui.client.widget.form.TextField;
+import com.extjs.gxt.ui.client.widget.form.Validator;
+import com.extjs.gxt.ui.client.widget.layout.FlowLayout;
+import com.extjs.gxt.ui.client.widget.layout.RowData;
+import com.extjs.gxt.ui.client.widget.layout.RowLayout;
+import com.extjs.gxt.ui.client.widget.form.FormPanel;
+import com.extjs.gxt.ui.client.widget.layout.FormData;
 
 public class ProfilesWindow extends Window {
 
@@ -22,43 +30,87 @@ public class ProfilesWindow extends Window {
 		setHeading("Gesti\u00F3n de Perfiles");
 		setLayout(new RowLayout(Orientation.VERTICAL));
 		
-		ButtonBar buttonBar = new ButtonBar();
-		
-		Button btnAdd = new Button("Add");
-		buttonBar.add(btnAdd);
-		add(buttonBar);
-		
 		TabPanel tabPanel = new TabPanel();
 		
 		TabItem tbtmNewTabitem = new TabItem("Detalles");
-		tbtmNewTabitem.setLayout(new RowLayout(Orientation.HORIZONTAL));
+		tbtmNewTabitem.setLayout(new FlowLayout(5));
 		
 		HorizontalPanel horizontalPanel = new HorizontalPanel();
 		
+		final FormPanel frmDetalles = new FormPanel();
+		frmDetalles.setHeaderVisible(false);
+		frmDetalles.setHeading("New FormPanel");
+		frmDetalles.setCollapsible(true);
+		
+		VerticalPanel verticalPanel = new VerticalPanel();
+		frmDetalles.add(verticalPanel, new FormData("100%"));
+		verticalPanel.setBorders(true);
+		
+		HorizontalPanel horizontalPanel_1 = new HorizontalPanel();
+		
+		LabelField lblfldNombre = new LabelField("Nombre");
+		horizontalPanel_1.add(lblfldNombre);
+		lblfldNombre.setWidth("75px");
+		
+		final TextField campoNombre = new TextField();
+		horizontalPanel_1.add(campoNombre);
+		campoNombre.setFieldLabel("New TextField");
+		verticalPanel.add(horizontalPanel_1);
+		horizontalPanel_1.setWidth("268px");
+		campoNombre.setValidator(new ValidaObligatorio());
+		campoNombre.setAutoValidate(Boolean.TRUE);
+		campoNombre.setAllowBlank(Boolean.FALSE);
+		
+		HorizontalPanel horizontalPanel_2 = new HorizontalPanel();
+		
+		LabelField lblfldConexion = new LabelField("Conexion");
+		horizontalPanel_2.add(lblfldConexion);
+		lblfldConexion.setWidth("75px");
+		
+		TextField campoConexion = new TextField();
+		campoConexion.setFieldLabel("New TextField");
+		horizontalPanel_2.add(campoConexion);
+		verticalPanel.add(horizontalPanel_2);
+		horizontalPanel_2.setWidth("268px");
+		
+		HorizontalPanel horizontalPanel_3 = new HorizontalPanel();
+		
+		LabelField lblfldGrupoLdap = new LabelField("Grupo LDAP");
+		horizontalPanel_3.add(lblfldGrupoLdap);
+		lblfldGrupoLdap.setWidth("75px");
+		
+		TextField campoGrupo = new TextField();
+		campoGrupo.setFieldLabel("New TextField");
+		horizontalPanel_3.add(campoGrupo);
+		verticalPanel.add(horizontalPanel_3);
+		horizontalPanel_3.setWidth("268px");
+		
+		Button btnAgregar = new Button("Agregar");
+		btnAgregar.addSelectionListener(new SelectionListener<ButtonEvent>() {
+			public void componentSelected(ButtonEvent ce) {
+				Info.display("Boton Agregar", "Validando campos");
+				if (!frmDetalles.isValid()){
+					Info.display("Formulario", "Existen campos invalidos");
+				} else {
+					Info.display("Formulario", "Validaciones correctas");
+				}
+			}
+		});
+		verticalPanel.add(btnAgregar);
+		
+		Button btnCancelar = new Button("Cancelar");
+		verticalPanel.add(btnCancelar);
+		verticalPanel.setSize("270px", "124px");
+		horizontalPanel.add(frmDetalles);
+		
 		VerticalPanel verticalPanel_1 = new VerticalPanel();
-		
-		Text txtNombre = new Text("Nombre");
-		verticalPanel_1.add(txtNombre);
+		verticalPanel_1.setBorders(true);
 		horizontalPanel.add(verticalPanel_1);
-		verticalPanel_1.setSize("97px", "31px");
-		
-		VerticalPanel verticalPanel_2 = new VerticalPanel();
-		
-		TextField textField = new TextField();
-		verticalPanel_2.add(textField);
-		horizontalPanel.add(verticalPanel_2);
-		verticalPanel_2.setHeight("31px");
-		tbtmNewTabitem.add(horizontalPanel, new RowData(800.0, 20.0, new Margins()));
+		verticalPanel_1.setSize("536px", "225px");
+		tbtmNewTabitem.add(horizontalPanel);
 		tabPanel.add(tbtmNewTabitem);
-		tbtmNewTabitem.setSize("770px", "148px");
-		add(tabPanel, new RowData(Style.DEFAULT, 200.0, new Margins()));
-		
-		TabPanel tabPanel_1 = new TabPanel();
-		
-		TabItem tbtmListadoDePerfiles = new TabItem("Listado de Perfiles");
-		tabPanel_1.add(tbtmListadoDePerfiles);
-		tbtmListadoDePerfiles.setSize("770px", "148px");
-		add(tabPanel_1, new RowData(Style.DEFAULT, 230.0, new Margins()));
+		tbtmNewTabitem.setWidth("813px");
+		add(tabPanel, new RowData(Style.DEFAULT, 265.0, new Margins()));
 	}
 
 }
