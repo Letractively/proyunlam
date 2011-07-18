2cde6a2d9cd17682e9b835a28764516e7ff8741a
macioro <macioro@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-07-18 23:29:16 +0000

    Modificacion en la tabla para guardar las tareas.

--- trunk/iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/client/IEvenTask.java
+++ trunk/iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/client/IEvenTask.java
@@ -33,27 +33,30 @@ import com.google.gwt.user.client.ui.SimplePanel;
 import com.extjs.gxt.ui.client.widget.button.ToggleButton;
 import com.extjs.gxt.ui.client.event.WidgetListener;
 import com.extjs.gxt.ui.client.event.ComponentEvent;
+import com.google.gwt.user.client.ui.FlexTable;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class IEvenTask implements EntryPoint {
 
+	private FlexTable	taskFlexTable	= new FlexTable();
+
 	public void onModuleLoad() {
 		RootPanel rootPanel = RootPanel.get();
 		rootPanel.setStyleName("dialogVPanel");
 		rootPanel.setSize("1024", "768");
-		
+
 		LoginWindow loginWindow = new LoginWindow();
 		loginWindow.show();
-		
+
 		MenuBar menuBar = new MenuBar();
-		
+
 		Menu menu = new Menu();
 		menuBar.setContextMenu(menu);
-		
+
 		Menu menu_1 = new Menu();
-		
+
 		MenuItem mntmPerfiles = new MenuItem("Perfiles");
 		mntmPerfiles.addSelectionListener(new SelectionListener<MenuEvent>() {
 			public void componentSelected(MenuEvent ce) {
@@ -62,44 +65,44 @@ public class IEvenTask implements EntryPoint {
 			}
 		});
 		menu_1.add(mntmPerfiles);
-		
+
 		MenuItem mntmAcciones = new MenuItem("Acciones");
 		menu_1.add(mntmAcciones);
-		
+
 		MenuItem mntmPreferencias = new MenuItem("Preferencias");
 		menu_1.add(mntmPreferencias);
 		MenuBarItem mnbrtmNewMenubaritem = new MenuBarItem("Archivo", menu_1);
 		menuBar.add(mnbrtmNewMenubaritem);
-		
+
 		Menu menu_2 = new Menu();
-		
+
 		MenuItem mntmBuscar = new MenuItem("Buscar");
 		menu_2.add(mntmBuscar);
-		
+
 		MenuItem mntmBorrar = new MenuItem("Borrar");
 		menu_2.add(mntmBorrar);
 		MenuBarItem mnbrtmEdicion = new MenuBarItem("Edicion", menu_2);
 		menuBar.add(mnbrtmEdicion);
-		
+
 		Menu menu_3 = new Menu();
-		
+
 		MenuItem mntmMostrarFiltros = new MenuItem("Mostrar Filtros");
 		menu_3.add(mntmMostrarFiltros);
 		MenuBarItem mnbrtmVista = new MenuBarItem("Vista", menu_3);
 		menuBar.add(mnbrtmVista);
-		
+
 		Menu menu_4 = new Menu();
-		
+
 		MenuItem mntmExportar = new MenuItem("Exportar...");
 		menu_4.add(mntmExportar);
 		MenuBarItem mnbrtmHerramientas = new MenuBarItem("Herramientas", menu_4);
 		menuBar.add(mnbrtmHerramientas);
-		
+
 		Menu menu_5 = new Menu();
-		
+
 		MenuItem mntmAyudaEnLinea = new MenuItem("Ayuda en linea");
 		menu_5.add(mntmAyudaEnLinea);
-		
+
 		MenuItem mntmAcercaDeIeventask = new MenuItem("Acerca de iEvenTask");
 		menu_5.add(mntmAcercaDeIeventask);
 		MenuBarItem mnbrtmAyuda = new MenuBarItem("Ayuda", menu_5);
@@ -107,20 +110,15 @@ public class IEvenTask implements EntryPoint {
 		rootPanel.add(menuBar);
 		menuBar.setSize("578px", "18px");
 		rootPanel.setWidgetPosition(menuBar, 0, 0);
-		
+
 		LayoutContainer layoutContainer = new LayoutContainer();
 		rootPanel.add(layoutContainer);
 		rootPanel.setWidgetPosition(layoutContainer, 584, 0);
 		layoutContainer.setSize("164px", "466px");
 		layoutContainer.setBorders(true);
-		
-		TabPanel tabPanel = new TabPanel();
-		rootPanel.add(tabPanel);
-		rootPanel.setWidgetPosition(tabPanel, 0, 81);
-		tabPanel.setSize("578px", "387px");
-		
+
 		ToolBar toolBar = new ToolBar();
-		
+
 		Button btnNuevaTarea = new Button("Nueva Tarea");
 		btnNuevaTarea.addSelectionListener(new SelectionListener<ButtonEvent>() {
 			public void componentSelected(ButtonEvent be) {
@@ -129,7 +127,7 @@ public class IEvenTask implements EntryPoint {
 			}
 		});
 		toolBar.add(btnNuevaTarea);
-		
+
 		Button btnNuevoObjetivo = new Button("Nuevo Objetivo");
 		btnNuevoObjetivo.addSelectionListener(new SelectionListener<ButtonEvent>() {
 			public void componentSelected(ButtonEvent be) {
@@ -141,5 +139,24 @@ public class IEvenTask implements EntryPoint {
 		rootPanel.add(toolBar);
 		rootPanel.setWidgetPosition(toolBar, 0, 24);
 		toolBar.setSize("562px", "34px");
+
+		// Crea la tabla para guardar las tareas
+		taskFlexTable.setText(0, 0, "ID");
+		taskFlexTable.setText(0, 1, "Nombre");
+		taskFlexTable.setText(0, 2, "Fecha Comienzo");
+		taskFlexTable.setText(0, 3, "Fecha Fin");
+		taskFlexTable.setText(0, 4, "Duracion");
+		taskFlexTable.setText(0, 5, "Descripcion");
+
+		// Agrega estilos a los elementos de la tabla de tareas.
+		taskFlexTable.setCellPadding(6);
+		taskFlexTable.getRowFormatter().addStyleName(0, "watchListHeader");
+		taskFlexTable.addStyleName("watchList");
+		taskFlexTable.getCellFormatter().addStyleName(0, 1, "watchListNumericColumn");
+		taskFlexTable.getCellFormatter().addStyleName(0, 2, "watchListNumericColumn");
+		taskFlexTable.getCellFormatter().addStyleName(0, 3, "watchListRemoveColumn");
+		
+
+		rootPanel.add(taskFlexTable, 10, 80);
 	}
 }
