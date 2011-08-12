497b9675cbca05898fac97047b5ac75e2570b4d8
llarreta <llarreta@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-08-12 13:22:25 +0000

    Se agregan scripts para tablas relacionadas con las tareas

--- iEvenTaskBackEnd/startup/CreateTables.sql
+++ iEvenTaskBackEnd/startup/CreateTables.sql
@@ -26,3 +26,52 @@ ADD CONSTRAINT fk_perp_perfil FOREIGN KEY (id_perfil) REFERENCES iet_perfil(id_p
 
 ALTER TABLE iet_permiso_perfil 
 ADD CONSTRAINT pk_perp PRIMARY KEY (id_permiso, id_perfil);
+
+create table iet_tarea (
+	id_tarea int primary key,
+	fecha_inicio datetime,
+	fecha_fin datetime,
+	horas_asignadas int,
+	id_usuario varchar(255),
+	fecha_creacion datetime,
+	fecha_modificacion datetime,
+	estado int,
+	cumplimiento int,
+	tipo_tarea int
+);
+create table iet_subtarea (
+	id_tarea int not null,
+	id_subtarea int not null
+);
+ALTER TABLE iet_subtarea 
+ADD CONSTRAINT fk_subtarea_tarea FOREIGN KEY (id_tarea) REFERENCES iet_tarea(id_tarea);
+
+ALTER TABLE iet_subtarea 
+ADD CONSTRAINT fk_subtarea_subtarea FOREIGN KEY (id_subtarea) REFERENCES iet_tarea(id_tarea);
+
+ALTER TABLE iet_subtarea 
+ADD CONSTRAINT pk_subtarea PRIMARY KEY (id_tarea, id_subtarea);
+
+create table iet_comentario (
+	id_comentario int not null,
+	id_tarea int,
+	comentario varchar(8000)
+);
+
+ALTER TABLE iet_comentario 
+ADD CONSTRAINT fk_comentario_tarea FOREIGN KEY (id_tarea) REFERENCES iet_tarea(id_tarea);
+
+ALTER TABLE iet_comentario 
+ADD CONSTRAINT pk_comentario PRIMARY KEY (id_comentario);
+
+create table iet_visualiza (
+	id_tarea int not null,
+	id_usuario varchar(255) not null
+);
+
+ALTER TABLE iet_visualiza 
+ADD CONSTRAINT fk_visualiza_tarea FOREIGN KEY (id_tarea) REFERENCES iet_tarea(id_tarea);
+
+ALTER TABLE iet_visualiza 
+ADD CONSTRAINT pk_visualiza PRIMARY KEY (id_tarea, id_usuario);
+
--- iEvenTaskBackEnd/startup/DropTables.sql
+++ iEvenTaskBackEnd/startup/DropTables.sql
@@ -7,3 +7,22 @@ ALTER TABLE iet_permiso_perfil
 DROP CONSTRAINT fk_perp_perfil;
 
 DROP TABLE iet_permiso_perfil;
+
+ALTER TABLE iet_visualiza 
+DROP CONSTRAINT fk_visualiza_tarea;
+
+DROP TABLE iet_visualiza;
+
+ALTER TABLE iet_comentario 
+DROP CONSTRAINT fk_comentario_tarea;
+
+DROP TABLE iet_comentario;
+
+ALTER TABLE iet_subtarea 
+DROP CONSTRAINT fk_subtarea_tarea;
+ALTER TABLE iet_subtarea 
+DROP CONSTRAINT fk_subtarea_subtarea;
+
+DROP TABLE iet_subtarea;
+
+DROP TABLE iet_tarea;
