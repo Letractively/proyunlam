b305a694cab9c2ad6d82a0f8909494ba7f4ab669
macioro <macioro@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-08-25 01:15:27 +0000

    Servicios para la creacion, modificacion y listado de tareas.

---
+++ iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/services/ListTaskService.java
@@ -0,0 +1,11 @@
+package ar.com.AmberSoft.iEvenTask.services;
+
+public class ListTaskService extends ListService {
+
+	@Override
+	protected String getEntity() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
