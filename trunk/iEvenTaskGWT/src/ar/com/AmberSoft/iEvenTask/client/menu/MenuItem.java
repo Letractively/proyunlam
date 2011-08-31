2ecc1ac50e15f5a19013747a05d4f46900543315
llarreta <llarreta@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-08-31 01:52:31 +0000

    Validacion para que solo muestre una ventana de cada tipo

--- trunk/iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/client/menu/MenuItem.java
+++ trunk/iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/client/menu/MenuItem.java
@@ -1,23 +1,31 @@
 package ar.com.AmberSoft.iEvenTask.client.menu;
 
+import ar.com.AmberSoft.iEvenTask.client.Context;
+import ar.com.AmberSoft.iEvenTask.client.Window;
 import ar.com.AmberSoft.iEvenTask.client.exceptions.WindowPreviouslyRegisteredException;
 
 import com.extjs.gxt.ui.client.event.MenuEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
-import com.extjs.gxt.ui.client.widget.Window;
-
 
 public class MenuItem extends com.extjs.gxt.ui.client.widget.menu.MenuItem {
+	
+	protected Context context = Context.getInstance();
 
 	public MenuItem(String text, final Window window){
 		super(text);
 		addSelectionListener(new SelectionListener<MenuEvent>() {
+			/**
+			 * Registra la ventana cuando se da click en la opcion
+			 * Si la ventana ya se encuentra registrada, toma esta para mostrar
+			 */
 			public void componentSelected(MenuEvent ce) {
+				Window windowContextRegistered = window;
 				try {
-					window.show();
+					context.registerWindow(window);
 				} catch (WindowPreviouslyRegisteredException e){
-					// FIXME: Mostrar por interfaz grafica un mensaje de error
+					windowContextRegistered = Context.getInstance().getWindowInstance(window.getClass());
 				}
+				windowContextRegistered.show();
 			}
 		});
 	}
