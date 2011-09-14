d3e92db0710a45faf7ed69e38207ea0dfdd545d7
macioro <macioro@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-09-14 01:42:39 +0000

    desarrollo de la creacion de tareas

--- iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/backend/entities/Comentario.java
+++ iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/backend/entities/Comentario.java
@@ -3,31 +3,48 @@ package ar.com.AmberSoft.iEvenTask.backend.entities;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import ar.com.AmberSoft.util.PKGenerator;
 
 @Entity
 @Table (name="iet_comentario")
+@Inheritance(strategy=InheritanceType.JOINED)
 public class Comentario {
-	private int id_comentario;
-	private int id_tarea;
+	private Integer id_comentario;
+	private Tarea task;
 	private String comentario;
 	
+	public Comentario(){
+		PKGenerator pkGenerator = new PKGenerator();
+		id_comentario = new Integer(pkGenerator.getIntLastTime());
+	}
+	
 	@Id @Column(name="id_comentario")
-	public int getId_comentario() {
+	public Integer getId_comentario() {
 		return id_comentario;
 	}
-	public void setId_comentario(int id_comentario) {
+
+	public void setId_comentario(Integer id_comentario) {
 		this.id_comentario = id_comentario;
 	}
-	@Basic @Column(name="id_tarea")
-	public int getId_tarea() {
-		return id_tarea;
+
+	@ManyToOne (fetch=FetchType.LAZY)
+	@JoinColumn (name="id_tarea")
+	public Tarea getTask() {
+		return task;
 	}
-	public void setId_tarea(int id_tarea) {
-		this.id_tarea = id_tarea;
+
+	public void setTask(Tarea task) {
+		this.task = task;
 	}
-	@Basic @Column(name="comentario")
+
 	public String getComentario() {
 		return comentario;
 	}
