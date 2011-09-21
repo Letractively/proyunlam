8fb22cdb2c7931561c2f1b096f3078ad8cfade14
llarreta <llarreta@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-09-21 02:07:16 +0000

    Se agrega boton hacia ventana de ejecucion de contexto

--- iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/client/menu/MainStatusBar.java
+++ iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/client/menu/MainStatusBar.java
@@ -1,7 +1,13 @@
 package ar.com.AmberSoft.iEvenTask.client.menu;
 
+import ar.com.AmberSoft.iEvenTask.client.Context;
+import ar.com.AmberSoft.iEvenTask.client.ExecutionDetailWindow;
 import ar.com.AmberSoft.iEvenTask.client.IEvenTask;
+import ar.com.AmberSoft.iEvenTask.client.resources.Resources;
 
+import com.extjs.gxt.ui.client.event.ButtonEvent;
+import com.extjs.gxt.ui.client.event.SelectionListener;
+import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
 import com.extjs.gxt.ui.client.widget.Status;
 import com.extjs.gxt.ui.client.widget.toolbar.FillToolItem;
@@ -18,6 +24,21 @@ public class MainStatusBar extends ToolBar  {
 	    status.setText("Not writing");  
 	    this.add(status);  
 	    this.add(new FillToolItem());
+	    
+	    
+	    Button executionDetailButton = new Button();
+	    executionDetailButton.setIcon(Resources.ICONS.text());
+	    executionDetailButton.setToolTip("Detalles de ejecucion");
+	    
+	    executionDetailButton.addSelectionListener(new SelectionListener<ButtonEvent>() {
+			@Override
+			public void componentSelected(ButtonEvent ce) {
+				Context.getInstance().windowShow(new ExecutionDetailWindow());
+			}
+		});
+	    
+	    add(executionDetailButton);
+	    
 	}
 
 }
