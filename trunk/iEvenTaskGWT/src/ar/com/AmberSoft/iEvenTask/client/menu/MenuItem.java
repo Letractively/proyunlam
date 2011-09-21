b77954954e047717271591492be8e259c46725d7
llarreta <llarreta@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-09-21 01:42:01 +0000

    Se cambia la forma de control de la apertura de ventanas

--- trunk/iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/client/menu/MenuItem.java
+++ trunk/iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/client/menu/MenuItem.java
@@ -2,15 +2,12 @@ package ar.com.AmberSoft.iEvenTask.client.menu;
 
 import ar.com.AmberSoft.iEvenTask.client.Context;
 import ar.com.AmberSoft.iEvenTask.client.Window;
-import ar.com.AmberSoft.iEvenTask.client.exceptions.WindowPreviouslyRegisteredException;
 
 import com.extjs.gxt.ui.client.event.MenuEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
 
 public class MenuItem extends com.extjs.gxt.ui.client.widget.menu.MenuItem {
 	
-	protected Context context = Context.getInstance();
-
 	public MenuItem(String text, final Window window){
 		super(text);
 		addSelectionListener(new SelectionListener<MenuEvent>() {
@@ -19,13 +16,7 @@ public class MenuItem extends com.extjs.gxt.ui.client.widget.menu.MenuItem {
 			 * Si la ventana ya se encuentra registrada, toma esta para mostrar
 			 */
 			public void componentSelected(MenuEvent ce) {
-				Window windowContextRegistered = window;
-				try {
-					context.registerWindow(window);
-				} catch (WindowPreviouslyRegisteredException e){
-					windowContextRegistered = Context.getInstance().getWindowInstance(window.getClass());
-				}
-				windowContextRegistered.show();
+				 Context.getInstance().windowShow(window);
 			}
 		});
 	}
