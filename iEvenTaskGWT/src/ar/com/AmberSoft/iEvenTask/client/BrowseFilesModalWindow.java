314aaa1c161b7ce45deea531f85946fa6d095481
llarreta <llarreta@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-09-22 02:49:46 +0000

    Busqueda en el filesystem del servidor

---
+++ iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/client/BrowseFilesModalWindow.java
@@ -0,0 +1,65 @@
+package ar.com.AmberSoft.iEvenTask.client;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import ar.com.AmberSoft.iEvenTask.client.utils.TreeGrid;
+import ar.com.AmberSoft.iEvenTask.shared.ParamsConst;
+import ar.com.AmberSoft.iEvenTask.shared.ServiceNameConst;
+
+import com.extjs.gxt.ui.client.data.BaseModel;
+import com.extjs.gxt.ui.client.event.BaseEvent;
+import com.extjs.gxt.ui.client.event.Events;
+import com.extjs.gxt.ui.client.event.Listener;
+import com.extjs.gxt.ui.client.widget.Window;
+import com.extjs.gxt.ui.client.widget.form.Field;
+import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
+import com.extjs.gxt.ui.client.widget.treegrid.TreeGridCellRenderer;
+
+public class BrowseFilesModalWindow extends Window {
+
+	private final Field fieldValueReturn;
+	
+	public BrowseFilesModalWindow(Field field) {
+		super();
+		this.fieldValueReturn = field;
+		this.setModal(Boolean.TRUE);
+		final TreeGrid treeGrid = new TreeGrid(ServiceNameConst.BROWSE_FILE, getTreeGridConfig());
+		treeGrid.setSize(255, 300);
+		setSize(260, 330);
+		add(treeGrid);
+		
+		// Acciones a realizar cuando selecciona algun registro de la grilla
+		treeGrid.getSelectionModel().addListener(Events.SelectionChange,
+				new Listener() {
+					@Override
+					public void handleEvent(BaseEvent be) {
+						List seleccionados = treeGrid.getSelectionModel().getSelection();
+						if (seleccionados.size() == 1) {
+							Iterator it = seleccionados.iterator();
+							if (it.hasNext()) {
+								BaseModel actual = (BaseModel) it.next();
+								fieldValueReturn.setValue(actual.get(ParamsConst.PATH));
+								close();
+							}
+						} 
+					}
+				});
+		
+	}
+
+	
+	/**
+	 * Retorna la configuracion de la grilla de archivos
+	 */
+	private List getTreeGridConfig() {
+		List<ColumnConfig> configs = new ArrayList<ColumnConfig>();
+
+		ColumnConfig clmncnfgPath = new ColumnConfig(ParamsConst.PATH, "Buscar...", 250);
+		clmncnfgPath.setRenderer(new TreeGridCellRenderer());
+		configs.add(clmncnfgPath);
+
+		return configs;
+	}
+}
