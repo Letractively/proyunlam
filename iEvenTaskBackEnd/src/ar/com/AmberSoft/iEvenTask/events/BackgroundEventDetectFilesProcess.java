021f0aa287654f3df21712901cd96ff0773e7013
llarreta <llarreta@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-09-22 19:18:58 +0000

    Se finaliza con la deteccion de creacion y modificacion de archivos Queda pendiente generar la tarea asociada tras la deteccion

--- iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/events/BackgroundEventDetectFilesProcess.java
+++ iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/events/BackgroundEventDetectFilesProcess.java
@@ -1,9 +1,21 @@
 package ar.com.AmberSoft.iEvenTask.events;
 
+import java.io.File;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
 import ar.com.AmberSoft.iEvenTask.backend.entities.Event;
+import ar.com.AmberSoft.iEvenTask.backend.entities.EventFiles;
+import ar.com.AmberSoft.iEvenTask.services.UpdateEntityService;
+import ar.com.AmberSoft.util.ParamsConst;
 
 public class BackgroundEventDetectFilesProcess extends
 		BackgroundEventDetectProcess {
+	
+	private static Logger logger = Logger.getLogger(BackgroundEventDetectFilesProcess.class);
 
 	public BackgroundEventDetectFilesProcess(Event event) {
 		super(event);
@@ -11,6 +23,33 @@ public class BackgroundEventDetectFilesProcess extends
 
 	@Override
 	public void eventDetect() {
+		logger.debug("Iniciando deteccion de archivos");
+		File file = new File(getEvent().getPath());
+		// Se esta detectando una creacion
+		if (getEvent().getControlType() == 1){
+			if (file.exists()){
+				logger.debug("Se detecta la creacion del archivo:" + getEvent().getPath());
+				//FIXME: Aca se deberia crear la nueva tarea asociada
+			}
+		} else {
+			// Se esta detectando una modificacion
+			Date lastModified = new Date(file.lastModified());
+			if ((getEvent().getLastModification()!=null) && (!getEvent().getLastModification().equals(lastModified))){
+				logger.debug("Se detecta la modificacion del archivo:" + getEvent().getPath());
+				//FIXME: Aca se deberia crear la nueva tarea asociada
+			} 
+			getEvent().setLastModification(lastModified);
+			UpdateEntityService entityService = new UpdateEntityService();
+			Map params = new HashMap();
+			params.put(ParamsConst.ENTITY, getEvent());
+			entityService.execute(params);
+		}
+		
+		logger.debug("Fin deteccion de archivos");
+	}
+	
+	public EventFiles getEvent(){
+		return (EventFiles) event;
 	}
 
 }
