5a4e4a137c464585d69bb637eb03317513216d6e
macioro <macioro@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-09-22 11:52:07 +0000

    desarrollo de la creacion de tareas

--- trunk/iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/services/ListTaskService.java
+++ trunk/iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/services/ListTaskService.java
@@ -2,20 +2,17 @@ package ar.com.AmberSoft.iEvenTask.services;
 
 import java.util.Map;
 
+import ar.com.AmberSoft.iEvenTask.backend.entities.Tarea;
+
 public class ListTaskService extends ListService {
 
 	@Override
 	protected String getEntity() {
-		// TODO Auto-generated method stub
-		return null;
+		return Tarea.class.getName();
 	}
 
 	@Override
 	public Map onEmulate(Map params) {
-		// TODO Auto-generated method stub
 		return null;
 	}
-
-
-
 }
