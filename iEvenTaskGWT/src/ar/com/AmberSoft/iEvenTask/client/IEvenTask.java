035fc515ae27f7e8a15f8f1ba6e136aa7623e572
macioro <macioro@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-09-22 11:53:55 +0000

    Desarrollo funcion Login.

--- iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/client/IEvenTask.java
+++ iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/client/IEvenTask.java
@@ -8,6 +8,7 @@ import ar.com.AmberSoft.iEvenTask.client.menu.MainToolBar;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -27,50 +28,55 @@ public class IEvenTask implements EntryPoint {
 		rootPanel.setStyleName("body");
 		rootPanel.setSize(APP_WINDOW_WIDTH.toString(), APP_WINDOW_HEIGTH.toString());
 		
-//		LoginPanel loginPanel = new LoginPanel();
-//		loginPanel.setId("login_panel");
-//		rootPanel.add(loginPanel);
-//		rootPanel.setWidgetPosition(loginPanel, 0, 0);
-		
-		iniciarSesion(true);
+		iniciarLogin();
+
 //		LoginWindow loginWindow = new LoginWindow();
 //		loginWindow.show();
 	}
 	
-	public static void iniciarSesion(boolean flag) {
-		if(flag){
-//			RootPanel.get("login_panel").setVisible(false);
-			//cargo el menu principal
-			MainMenu mainMenu = new MainMenu();
-			mainMenu.setId("main_menu");
-			rootPanel.add(mainMenu);
-			rootPanel.setWidgetPosition(mainMenu, 0, 0);
-			
-			//cargo la barra de botones
-			MainToolBar mainToolBar = new MainToolBar();
-			mainToolBar.setId("main_tool_bar");
-			rootPanel.add(mainToolBar);
-			rootPanel.setWidgetPosition(mainToolBar, 0, DEFAULT_MENU_HEIGTH);
-			
-			//cargo el panel tab
-			MainTabPanel mainTabPanel = new MainTabPanel();
-			mainTabPanel.setId("main_tab_panel");
-			rootPanel.add(mainTabPanel);
-			rootPanel.setWidgetPosition(mainTabPanel, 0, new Integer(2*DEFAULT_MENU_HEIGTH));
-			
-			//cargo la barra de estado
-			MainStatusBar mainStatusBar = new MainStatusBar();
-			mainStatusBar.setId("main_status_bar");
-			rootPanel.add(mainStatusBar);
-			rootPanel.setWidgetPosition(mainStatusBar, 0, new Integer((2*DEFAULT_MENU_HEIGTH) + MAIN_TAB_PANEL_HEIGTH));
-		}
+	public static void iniciarSesion() {
+		//elimino del panel de logins
+		rootPanel.remove(RootPanel.get("login_panel"));
+		
+		//cargo el menu principal
+		MainMenu mainMenu = new MainMenu();
+		mainMenu.setId("main_menu");
+		rootPanel.add(mainMenu);
+		rootPanel.setWidgetPosition(mainMenu, 0, 0);
+		
+		//cargo la barra de botones
+		MainToolBar mainToolBar = new MainToolBar();
+		mainToolBar.setId("main_tool_bar");
+		rootPanel.add(mainToolBar);
+		rootPanel.setWidgetPosition(mainToolBar, 0, DEFAULT_MENU_HEIGTH);
+		
+		//cargo el panel tab
+		MainTabPanel mainTabPanel = new MainTabPanel();
+		mainTabPanel.setId("main_tab_panel");
+		rootPanel.add(mainTabPanel);
+		rootPanel.setWidgetPosition(mainTabPanel, 0, new Integer(2*DEFAULT_MENU_HEIGTH));
+		
+		//cargo la barra de estado
+		MainStatusBar mainStatusBar = new MainStatusBar();
+		mainStatusBar.setId("main_status_bar");
+		rootPanel.add(mainStatusBar);
+		rootPanel.setWidgetPosition(mainStatusBar, 0, new Integer((2*DEFAULT_MENU_HEIGTH) + MAIN_TAB_PANEL_HEIGTH));
 	}
 	
-	private static void cerrarSesion (boolean flag){
-		RootPanel.get("main_menu").setVisible(false);
-		RootPanel.get("main_tool_bar").setVisible(false);
-		RootPanel.get("main_tab_panel").setVisible(false);
-		RootPanel.get("main_status_bar").setVisible(false);
-		RootPanel.get("login_panel").setVisible(true);
+	public static void cerrarSesion(){
+        java.util.Iterator<Widget> itr = rootPanel.iterator();
+        while(itr.hasNext()) {
+                itr.next();
+                itr.remove();
+        }
+        iniciarLogin();
 	}
+	
+	public static void iniciarLogin(){
+		LoginPanel loginPanel = new LoginPanel();
+		loginPanel.setId("login_panel");
+		rootPanel.add(loginPanel);
+		rootPanel.setWidgetPosition(loginPanel, 0, 0);
+	}
+	
 }
--- iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/client/menu/MainMenu.java
+++ iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/client/menu/MainMenu.java
@@ -2,19 +2,60 @@ package ar.com.AmberSoft.iEvenTask.client.menu;
 
 import ar.com.AmberSoft.iEvenTask.client.IEvenTask;
 
+import com.extjs.gxt.ui.client.event.ButtonEvent;
+import com.extjs.gxt.ui.client.event.SelectionListener;
+import com.extjs.gxt.ui.client.widget.LayoutContainer;
 import com.extjs.gxt.ui.client.widget.menu.MenuBar;
 import com.extjs.gxt.ui.client.widget.menu.MenuBarItem;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.HorizontalPanel;
+import com.extjs.gxt.ui.client.widget.Info;
+import com.extjs.gxt.ui.client.widget.VerticalPanel;
 
-public class MainMenu extends MenuBar {
-
+public class MainMenu extends LayoutContainer {
+	
+	public static final Integer BUTTON_WIDTH = 100;
+	public static final Integer VERTICAL_PANEL_MENU_WIDTH = IEvenTask.APP_WINDOW_WIDTH-BUTTON_WIDTH;
+	
+	MenuBar mnuBar;
 	MenuBarItem mnuFile; 
+	private HorizontalPanel horizontalPanel;
+	private VerticalPanel verticalPanel;
+	private VerticalPanel verticalPanel_1;
 	
 	public MainMenu(){
 		super();
 		setSize(IEvenTask.APP_WINDOW_WIDTH.toString(), IEvenTask.DEFAULT_MENU_HEIGTH.toString());
 		
-		mnuFile = new MenuBarItem("Archivo", new FileMenu());
-		this.add(mnuFile);
+		horizontalPanel = new HorizontalPanel();
+		
+		verticalPanel = new VerticalPanel();
+		verticalPanel.setWidth(VERTICAL_PANEL_MENU_WIDTH);
+		mnuBar = new MenuBar();
+		verticalPanel.add(mnuBar);
+		FileMenu fileMenu = new FileMenu();
+		mnuFile = new MenuBarItem("Archivo", fileMenu);
+		mnuBar.add(mnuFile);
+		horizontalPanel.add(verticalPanel);
+		
+		verticalPanel_1 = new VerticalPanel();
+		
+		Button btnSalir = new Button("Salir");
+		btnSalir.addSelectionListener(new SelectionListener<ButtonEvent>(){
+			@Override
+			public void componentSelected(ButtonEvent ce)
+			{
+				cerrarSesion();
+			}});
+		btnSalir.setWidth(BUTTON_WIDTH);
+		verticalPanel_1.add(btnSalir);
+		horizontalPanel.add(verticalPanel_1);
+		add(horizontalPanel);
+	}
+	
+	protected void cerrarSesion() {
+		Info.display("entre a cerrarSesion", "");
+		IEvenTask.cerrarSesion();
 	}
 	
 }
