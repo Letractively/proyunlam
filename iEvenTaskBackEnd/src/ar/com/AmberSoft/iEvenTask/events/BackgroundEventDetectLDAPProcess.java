200b7f011758095d2a19a0f6127eaa941c538d04
llarreta <llarreta@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-09-23 01:37:11 +0000

    Se avanza con la deteccion de eventos

--- iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/events/BackgroundEventDetectLDAPProcess.java
+++ iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/events/BackgroundEventDetectLDAPProcess.java
@@ -1,17 +1,25 @@
 package ar.com.AmberSoft.iEvenTask.events;
 
+import org.apache.log4j.Logger;
+
 import ar.com.AmberSoft.iEvenTask.backend.entities.Event;
 
 public class BackgroundEventDetectLDAPProcess extends
 		BackgroundEventDetectProcess {
+	
+	private static Logger logger = Logger.getLogger(BackgroundEventDetectLDAPProcess.class);
 
 	public BackgroundEventDetectLDAPProcess(Event event) {
 		super(event);
 	}
 
 	@Override
-	public void eventDetect() {
-
+	public Boolean eventDetect() {
+		logger.debug("Iniciando deteccion de evento LDAP");
+		
+		
+		logger.debug("Fin deteccion de evento LDAP");
+		return null;
 	}
 
 }
--- iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/events/BackgroundEventDetectLogsProcess.java
+++ iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/events/BackgroundEventDetectLogsProcess.java
@@ -1,16 +1,58 @@
 package ar.com.AmberSoft.iEvenTask.events;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
 import ar.com.AmberSoft.iEvenTask.backend.entities.Event;
+import ar.com.AmberSoft.iEvenTask.backend.entities.EventLogs;
+import ar.com.AmberSoft.iEvenTask.services.UpdateEntityService;
+import ar.com.AmberSoft.iEvenTask.utils.Tools;
+import ar.com.AmberSoft.util.ParamsConst;
 
 public class BackgroundEventDetectLogsProcess extends
 		BackgroundEventDetectProcess {
+	
+	private static Logger logger = Logger.getLogger(BackgroundEventDetectLogsProcess.class);
 
 	public BackgroundEventDetectLogsProcess(Event event) {
 		super(event);
 	}
 
 	@Override
-	public void eventDetect() {
+	public Boolean eventDetect() {
+		logger.debug("Iniciando deteccion de logs");
+		Boolean detected = Boolean.FALSE;
+		File file = new File(getEvent().getPath());
+		
+		try {
+			if ((file.exists()) && (file.canRead())){
+				FileReader fileReader = new FileReader (file);
+				BufferedReader bufferedReader = new BufferedReader(fileReader);
+	
+		         // Lectura del fichero
+		         String line;
+		         while((line=bufferedReader.readLine())!=null) {
+		        	 //FIXME: Evaluar que conincida con el patron buscado
+		            System.out.println(line);
+		            
+		         }
+			}
+		} catch(Exception e){
+			logger.error(Tools.getStackTrace(e));
+		}
+		
+		logger.debug("Fin deteccion de logs");
+		return detected;
+	}
+	
+	public EventLogs getEvent(){
+		return (EventLogs) event;
 	}
 
 }
