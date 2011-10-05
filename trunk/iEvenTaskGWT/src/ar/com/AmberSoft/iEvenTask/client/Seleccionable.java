eeaef966dd3f3c72b0c91ab3cdb1a992ce3df8c6
llarreta <llarreta@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-10-05 18:45:01 +0000

    Interface utilizada por la grilla que tienen que implementar los componentes que quieran usarla

---
+++ trunk/iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/client/Seleccionable.java
@@ -0,0 +1,15 @@
+package ar.com.AmberSoft.iEvenTask.client;
+
+import java.util.List;
+
+public interface Seleccionable {
+	/**
+	 * Se invoca cuando se realiza una accion de borrado
+	 */
+	void onDelete();
+	
+	/**
+	 * Se invoca cuando se realiza una accion de seleccion
+	 */
+	void onSelect(List selected);
+}
