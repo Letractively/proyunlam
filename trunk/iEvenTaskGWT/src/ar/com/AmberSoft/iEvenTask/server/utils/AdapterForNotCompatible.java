3b25923684261926c69c9dfb417a65d76898698a
llarreta <llarreta@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-10-07 02:01:27 +0000

    Se valida que el objeto actual sea diferente de nulo

--- trunk/iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/server/utils/AdapterForNotCompatible.java
+++ trunk/iEvenTaskGWT/src/ar/com/AmberSoft/iEvenTask/server/utils/AdapterForNotCompatible.java
@@ -20,36 +20,38 @@ public class AdapterForNotCompatible implements Compatibilizable {
 	@Override
 	public Object adapt(Object actual) {
 		Map map = new HashMap();
-		try {
-			map.put(ParamsConst.CLASS, actual.getClass().getName());
-			Collection properties = Arrays.asList(PropertyUtils
-					.getPropertyDescriptors(actual));
-			Iterator itProperties = properties.iterator();
-			while (itProperties.hasNext()) {
-				PropertyDescriptor descriptor = (PropertyDescriptor) itProperties
-						.next();
-
-				if ((PropertyUtils.isWriteable(actual,
-						descriptor.getDisplayName()))
-						&& (PropertyUtils.isReadable(actual,
-								descriptor.getDisplayName()))) {
-					Object propertyValue = null;
-					try {
-						propertyValue = PropertyUtils.getProperty(actual, descriptor.getName());
-					} catch (Exception e){
-						logger.info("La propiedad " + descriptor.getName() + " se encuentra Lazy o existe un error al tratar de obtenerla y será ignorada");
+		if (actual != null){
+			try {
+				map.put(ParamsConst.CLASS, actual.getClass().getName());
+				Collection properties = Arrays.asList(PropertyUtils
+						.getPropertyDescriptors(actual));
+				Iterator itProperties = properties.iterator();
+				while (itProperties.hasNext()) {
+					PropertyDescriptor descriptor = (PropertyDescriptor) itProperties
+							.next();
+	
+					if ((PropertyUtils.isWriteable(actual,
+							descriptor.getDisplayName()))
+							&& (PropertyUtils.isReadable(actual,
+									descriptor.getDisplayName()))) {
+						Object propertyValue = null;
+						try {
+							propertyValue = PropertyUtils.getProperty(actual, descriptor.getName());
+						} catch (Exception e){
+							logger.info("La propiedad " + descriptor.getName() + " se encuentra Lazy o existe un error al tratar de obtenerla y será ignorada");
+						}
+						GWTCompatibilityEvaluatorTypes evaluatorTypes = new GWTCompatibilityEvaluatorTypes(propertyValue);
+						Compatibilizable compatibilizable = evaluatorTypes.getCompatibilizableAdapter();
+						if (propertyValue!=null){
+							propertyValue = compatibilizable.adapt(propertyValue);
+						}
+						map.put(descriptor.getName(), propertyValue);
 					}
-					GWTCompatibilityEvaluatorTypes evaluatorTypes = new GWTCompatibilityEvaluatorTypes(propertyValue);
-					Compatibilizable compatibilizable = evaluatorTypes.getCompatibilizableAdapter();
-					if (propertyValue!=null){
-						propertyValue = compatibilizable.adapt(propertyValue);
-					}
-					map.put(descriptor.getName(), propertyValue);
 				}
+	
+			} catch (Exception e) {
+				logger.error(Tools.getStackTrace(e));
 			}
-
-		} catch (Exception e) {
-			logger.error(Tools.getStackTrace(e));
 		}
 
 		return map;
