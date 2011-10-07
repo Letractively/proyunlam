b6e291d0ceca0cdf1eead7954d3065902d798b0c
llarreta <llarreta@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-10-07 02:23:22 +0000

    listado de usuario de ldap

---
+++ trunk/iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/services/ListUserService.java
@@ -0,0 +1,60 @@
+package ar.com.AmberSoft.iEvenTask.services;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import ar.com.AmberSoft.iEvenTask.backend.entities.User;
+import ar.com.AmberSoft.util.LdapSearch;
+import ar.com.AmberSoft.util.PKGenerator;
+import ar.com.AmberSoft.util.ParamsConst;
+
+public class ListUserService extends Service {
+
+	@Override
+	public Map onExecute(Map params) {
+		
+		LdapSearch ldapSearch = new LdapSearch();
+		Collection<User> users = ldapSearch.search();
+		
+		Map map = new HashMap();
+		map.put(ParamsConst.DATA, users);
+		map.put(ParamsConst.TOTAL_COUNT, users.size());
+		//map.put(ParamsConst.OFFSET, null);
+		map.put(ParamsConst.PAGING_LOAD_RESULT, Boolean.TRUE);
+		return map;
+	}
+
+	@Override
+	public Map onEmulate(Map params) {
+
+		Collection<User> users = new ArrayList<User>();
+		
+		users.add(generateNewUser());
+		users.add(generateNewUser());
+		users.add(generateNewUser());
+		users.add(generateNewUser());
+		users.add(generateNewUser());
+		users.add(generateNewUser());
+		users.add(generateNewUser());
+		
+		Map map = new HashMap();
+		map.put(ParamsConst.DATA, users);
+		map.put(ParamsConst.TOTAL_COUNT, users.size());
+		//map.put(ParamsConst.OFFSET, null);
+		map.put(ParamsConst.PAGING_LOAD_RESULT, Boolean.TRUE);
+		return map;
+	}
+
+	public User generateNewUser() {
+		User user = new User();
+		PKGenerator pkGenerator = new PKGenerator();
+		user.setId(pkGenerator.getPk());
+		user.setName("Usuario" + pkGenerator.getPk());
+		user.setLastLogon(new Date());
+		return user;
+	}
+
+}
