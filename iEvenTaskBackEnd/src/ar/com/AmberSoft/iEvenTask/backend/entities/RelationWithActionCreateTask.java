fd458e4b8ff6d9e4aadef83b3e7ce0570d0fd469
llarreta <llarreta@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-10-07 03:22:20 +0000

    nuevo metodo de ejecucion para creacion o modificacion de tareas

--- iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/backend/entities/RelationWithActionCreateTask.java
+++ iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/backend/entities/RelationWithActionCreateTask.java
@@ -1,9 +1,15 @@
 package ar.com.AmberSoft.iEvenTask.backend.entities;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+
+import ar.com.AmberSoft.iEvenTask.services.CreateTaskService;
+import ar.com.AmberSoft.util.ParamsConst;
 
 @Entity
 @Table (name="iet_relacion_nueva_tarea")
@@ -26,6 +32,17 @@ public class RelationWithActionCreateTask extends Relation {
 	}
 	public void setUser(String user) {
 		this.user = user;
+	}
+	@Override
+	public void execute() {
+	
+		Map params = new HashMap();
+		params.put(ParamsConst.NOMBRE_TAREA, name);
+		params.put(ParamsConst.ID_USUARIO, user);
+		
+		CreateTaskService service = new CreateTaskService();
+		service.execute(params);
+		
 	}
 	
 	
