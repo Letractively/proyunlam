6bacc677e8d5a820b65fad1be9346a870a018faa
llarreta <llarreta@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-10-07 03:20:59 +0000

    Clase que representa a los usuarios de ldap

---
+++ iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/backend/entities/User.java
@@ -0,0 +1,63 @@
+package ar.com.AmberSoft.iEvenTask.backend.entities;
+
+import java.util.Date;
+
+public class User {
+
+	private String id;
+	private String name;
+	private Profile profile;
+	private Date created;
+	private Date changed;
+	private Date lastLogon;
+	private Date lastLogoff;
+	
+	
+	public Date getChanged() {
+		return changed;
+	}
+	public void setChanged(Date changed) {
+		this.changed = changed;
+	}
+	public Date getLastLogoff() {
+		return lastLogoff;
+	}
+	public void setLastLogoff(Date lastLogoff) {
+		this.lastLogoff = lastLogoff;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	public Profile getProfile() {
+		return profile;
+	}
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getLastLogon() {
+		return lastLogon;
+	}
+	public void setLastLogon(Date lastLogon) {
+		this.lastLogon = lastLogon;
+	}
+
+	
+	
+	
+	
+}
