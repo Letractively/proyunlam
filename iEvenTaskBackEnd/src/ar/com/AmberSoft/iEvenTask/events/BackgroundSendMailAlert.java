c55ddcdc96ba06ea6bbe6ab89a7382c28fc70255
llarreta <llarreta@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-10-12 12:25:50 +0000

    Clases encargadas del envio de emails

---
+++ iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/events/BackgroundSendMailAlert.java
@@ -0,0 +1,32 @@
+package ar.com.AmberSoft.iEvenTask.events;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.TimerTask;
+
+import org.apache.log4j.Logger;
+
+import ar.com.AmberSoft.util.Email;
+
+public class BackgroundSendMailAlert extends TimerTask{
+
+	private static Logger logger = Logger.getLogger(BackgroundSendMailAlert.class);
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	
+	public static void main(String []args){
+		Email email = new Email();
+		email.setMensaje(new StringBuffer("Hola Mundo"));
+		Collection<String> destinatarios = new ArrayList<String>();
+		destinatarios.add("llarreta@gmail.com");
+		email.setDestinatarios(destinatarios);
+		email.setAsunto("Probando");
+		email.enviar();
+	}
+}
