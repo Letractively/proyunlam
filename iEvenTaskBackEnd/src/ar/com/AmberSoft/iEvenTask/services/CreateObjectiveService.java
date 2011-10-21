5123c23cd10424c0d0e6256c806d08e59c2d0b51
macioro <macioro@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-10-21 17:09:01 +0000

    pruebas de objetivos

--- iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/services/CreateObjectiveService.java
+++ iEvenTaskBackEnd/src/ar/com/AmberSoft/iEvenTask/services/CreateObjectiveService.java
@@ -1,15 +1,20 @@
 package ar.com.AmberSoft.iEvenTask.services;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import ar.com.AmberSoft.iEvenTask.backend.entities.Entity;
 import ar.com.AmberSoft.iEvenTask.backend.entities.Objetivo;
+import ar.com.AmberSoft.iEvenTask.backend.entities.User;
 import ar.com.AmberSoft.util.ParamsConst;
 
 public class CreateObjectiveService extends CreateService {
 
-	@SuppressWarnings("rawtypes")
+	@SuppressWarnings({"rawtypes", "unchecked"})
 	@Override
 	public Entity getEntity(Map params) {
 		Objetivo objetivo = new Objetivo();
@@ -20,6 +25,22 @@ public class CreateObjectiveService extends CreateService {
 		objetivo.setPonderacion((Integer)params.get(ParamsConst.PONDERACION));
 		objetivo.setIdUsuarioAsignado((String) params.get(ParamsConst.ID_USUARIO_ASIGNADO));
 		objetivo.setDescripcion((String)params.get(ParamsConst.DESCRIPCION));
+		
+		HttpServletRequest request = (HttpServletRequest) params.get(ParamsConst.REQUEST);
+		User user = (User) request.getSession().getAttribute(ParamsConst.USER);
+		
+		objetivo.setCreator(user.getId());
+		
+		objetivo.defaultVisibles();
+		
+		Collection usersView = (Collection) params.get(ParamsConst.USERS_VIEW);
+		if (usersView!=null){
+			Iterator<String> itUsers = usersView.iterator();
+			while (itUsers.hasNext()) {
+				String actual = (String) itUsers.next();
+				objetivo.addVisible(actual);	
+			}
+		}
 		
 		return objetivo;
 	}
