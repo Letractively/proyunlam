aad5287eef8a6bbb002d35674d4b8384027b220d
llarreta <llarreta@1f46eabd-a8e4-850b-31b2-4da8121ca457>
2011-10-25 03:21:34 +0000

    se corrige problemas de ejecucion de los scripts

--- trunk/iEvenTaskBackEnd/startup/CreateTables.sql
+++ trunk/iEvenTaskBackEnd/startup/CreateTables.sql
@@ -1,3 +1,6 @@
+------------------------------------------------------------
+---                  Creates                             ---
+------------------------------------------------------------
 create table iet_perfil (
 	id_perfil int primary key, 
 	grupo_ldap char(150) unique,
@@ -9,32 +12,10 @@ create table iet_permiso (
 	descripcion varchar(255) not null
 );
 
-insert into iet_permiso values (1, 'Gestionar perfiles');
-insert into iet_permiso values (2, 'Gestionar eventos');
-insert into iet_permiso values (3, 'Crear y modificar tareas');
-insert into iet_permiso values (4, 'Crear y modificar tareas no asignadas');
-insert into iet_permiso values (5, 'Crear y modificar objetivos');
-insert into iet_permiso values (6, 'Crear y modificar objetivos no asignadas');
-insert into iet_permiso values (7, 'Agregar comentarios');
-insert into iet_permiso values (8, 'Agregar comentarios no asignadas');
-insert into iet_permiso values (9, 'Asignar Tareas');
-insert into iet_permiso values (10, 'Reasignar Tareas');
-insert into iet_permiso values (11, 'Subdividir Tareas');
-
 create table iet_permiso_perfil (
 	id_permiso int not null, 
 	id_perfil int not null
 );
-
-ALTER TABLE iet_permiso_perfil 
-ADD CONSTRAINT fk_perp_permiso FOREIGN KEY (id_permiso) REFERENCES iet_permiso(id_permiso);
-
-ALTER TABLE iet_permiso_perfil 
-ADD CONSTRAINT fk_perp_perfil FOREIGN KEY (id_perfil) REFERENCES iet_perfil(id_perfil);
-
-ALTER TABLE iet_permiso_perfil 
-ADD CONSTRAINT pk_perp PRIMARY KEY (id_permiso, id_perfil);
-
 
 create table iet_tarea (
 	id_tarea int primary key,
@@ -47,7 +28,7 @@ create table iet_tarea (
 	horas_asignadas int,
 	fecha_creacion datetime,
 	fecha_modificacion datetime,
-	estado varchar(20),
+	estado int,
 	cumplimiento int,
 	tipo_tarea int,
 	eliminado datetime
@@ -57,14 +38,6 @@ create table iet_subtarea (
 	id_tarea int not null,
 	id_subtarea int not null
 );
-ALTER TABLE iet_subtarea 
-ADD CONSTRAINT fk_subtarea_tarea FOREIGN KEY (id_tarea) REFERENCES iet_tarea(id_tarea);
-
-ALTER TABLE iet_subtarea 
-ADD CONSTRAINT fk_subtarea_subtarea FOREIGN KEY (id_subtarea) REFERENCES iet_tarea(id_tarea);
-
-ALTER TABLE iet_subtarea 
-ADD CONSTRAINT pk_subtarea PRIMARY KEY (id_tarea, id_subtarea);
 
 create table iet_comentario (
 	id_comentario int not null,
@@ -74,22 +47,10 @@ create table iet_comentario (
 	fecha datetime not null
 );
 
-ALTER TABLE iet_comentario 
-ADD CONSTRAINT fk_comentario_tarea FOREIGN KEY (id_tarea) REFERENCES iet_tarea(id_tarea);
-
-ALTER TABLE iet_comentario 
-ADD CONSTRAINT pk_comentario PRIMARY KEY (id_comentario);
-
 create table iet_visualiza (
 	id_tarea int not null,
 	id_usuario varchar(255) not null
 );
-
-ALTER TABLE iet_visualiza 
-ADD CONSTRAINT fk_visualiza_tarea FOREIGN KEY (id_tarea) REFERENCES iet_tarea(id_tarea);
-
-ALTER TABLE iet_visualiza 
-ADD CONSTRAINT pk_visualiza PRIMARY KEY (id_tarea, id_usuario);
 
 create table iet_evento (
 	id_evento int primary key, 
@@ -106,17 +67,11 @@ create table iet_evento_ldap (
 	codigo varchar(50) not null
 );
 
-ALTER TABLE iet_evento_ldap 
-ADD CONSTRAINT fk_evento_eventoldap FOREIGN KEY (id_evento) REFERENCES iet_evento(id_evento);
-
 create table iet_evento_logs (
 	id_evento int primary key,
 	ruta varchar(255) not null,
 	patron varchar(255) not null
 );
-
-ALTER TABLE iet_evento_logs 
-ADD CONSTRAINT fk_evento_eventologs FOREIGN KEY (id_evento) REFERENCES iet_evento(id_evento);
 
 create table iet_evento_archivos (
 	id_evento int primary key,
@@ -125,17 +80,11 @@ create table iet_evento_archivos (
 	modificacion datetime
 );
 
-ALTER TABLE iet_evento_archivos 
-ADD CONSTRAINT fk_evento_eventoarchivos FOREIGN KEY (id_evento) REFERENCES iet_evento(id_evento);
-
 create table iet_evento_servicios (
 	id_evento int primary key,
 	servidor varchar(255) not null,
 	port varchar(10) not null
 );
-
-ALTER TABLE iet_evento_servicios 
-ADD CONSTRAINT fk_evento_eventoservicios FOREIGN KEY (id_evento) REFERENCES iet_evento(id_evento);
 
 create table iet_relacion (
 	id_relacion int primary key,
@@ -156,15 +105,6 @@ create table iet_relacion_modifica_estado (
 	estado_final varchar(10) not null
 );
 
-ALTER TABLE iet_relacion 
-ADD CONSTRAINT fk_relacion_evento FOREIGN KEY (id_evento) REFERENCES iet_evento(id_evento);
-
-ALTER TABLE iet_relacion_nueva_tarea 
-ADD CONSTRAINT fk_relacion_nueva_tarea_relacion FOREIGN KEY (id_relacion) REFERENCES iet_relacion(id_relacion);
-
-ALTER TABLE iet_relacion_modifica_estado 
-ADD CONSTRAINT fk_relacion_modifica_estado_relacion FOREIGN KEY (id_relacion) REFERENCES iet_relacion(id_relacion);
-
 create table iet_objetivo (
 	id_objetivo int primary key,
 	tipo_objetivo varchar(20),
@@ -183,14 +123,99 @@ create table iet_visible (
 	id_usuario varchar(255)
 );
 
-ALTER TABLE iet_visible 
-ADD CONSTRAINT fk_relacion_tarea_visible FOREIGN KEY (id_tarea) REFERENCES iet_tarea(id_tarea);
-
 create table iet_obj_visible (
 	id_obj_visible varchar(1000) primary key,
 	id_objetivo int not null,
 	id_usuario varchar(255)
 );
 
+------------------------------------------------------------
+---                  Restrictions                        ---
+------------------------------------------------------------
+
+ALTER TABLE iet_permiso_perfil 
+ADD CONSTRAINT fk_perp_permiso FOREIGN KEY (id_permiso) REFERENCES iet_permiso(id_permiso);
+
+ALTER TABLE iet_permiso_perfil 
+ADD CONSTRAINT fk_perp_perfil FOREIGN KEY (id_perfil) REFERENCES iet_perfil(id_perfil);
+
+ALTER TABLE iet_permiso_perfil 
+ADD CONSTRAINT pk_perp PRIMARY KEY (id_permiso, id_perfil);
+
+ALTER TABLE iet_subtarea 
+ADD CONSTRAINT fk_subtarea_tarea FOREIGN KEY (id_tarea) REFERENCES iet_tarea(id_tarea);
+
+ALTER TABLE iet_subtarea 
+ADD CONSTRAINT fk_subtarea_subtarea FOREIGN KEY (id_subtarea) REFERENCES iet_tarea(id_tarea);
+
+ALTER TABLE iet_subtarea 
+ADD CONSTRAINT pk_subtarea PRIMARY KEY (id_tarea, id_subtarea);
+
+ALTER TABLE iet_comentario 
+ADD CONSTRAINT fk_comentario_tarea FOREIGN KEY (id_tarea) REFERENCES iet_tarea(id_tarea);
+
+ALTER TABLE iet_comentario 
+ADD CONSTRAINT pk_comentario PRIMARY KEY (id_comentario);
+
+ALTER TABLE iet_visualiza 
+ADD CONSTRAINT fk_visualiza_tarea FOREIGN KEY (id_tarea) REFERENCES iet_tarea(id_tarea);
+
+ALTER TABLE iet_visualiza 
+ADD CONSTRAINT pk_visualiza PRIMARY KEY (id_tarea, id_usuario);
+
+ALTER TABLE iet_evento_ldap 
+ADD CONSTRAINT fk_evento_eventoldap FOREIGN KEY (id_evento) REFERENCES iet_evento(id_evento);
+
+ALTER TABLE iet_evento_logs 
+ADD CONSTRAINT fk_evento_eventologs FOREIGN KEY (id_evento) REFERENCES iet_evento(id_evento);
+
+ALTER TABLE iet_evento_archivos 
+ADD CONSTRAINT fk_evento_eventoarchivos FOREIGN KEY (id_evento) REFERENCES iet_evento(id_evento);
+
+ALTER TABLE iet_evento_servicios 
+ADD CONSTRAINT fk_evento_eventoservicios FOREIGN KEY (id_evento) REFERENCES iet_evento(id_evento);
+
+ALTER TABLE iet_relacion 
+ADD CONSTRAINT fk_relacion_evento FOREIGN KEY (id_evento) REFERENCES iet_evento(id_evento);
+
+ALTER TABLE iet_relacion_nueva_tarea 
+ADD CONSTRAINT fk_relacion_nueva_tarea_relacion FOREIGN KEY (id_relacion) REFERENCES iet_relacion(id_relacion);
+
+ALTER TABLE iet_relacion_modifica_estado 
+ADD CONSTRAINT fk_relacion_modifica_estado_relacion FOREIGN KEY (id_relacion) REFERENCES iet_relacion(id_relacion);
+
+ALTER TABLE iet_visible 
+ADD CONSTRAINT fk_relacion_tarea_visible FOREIGN KEY (id_tarea) REFERENCES iet_tarea(id_tarea);
+
 ALTER TABLE iet_obj_visible 
 ADD CONSTRAINT fk_relacion_objetivo_visible FOREIGN KEY (id_objetivo) REFERENCES iet_objetivo(id_objetivo);
+
+------------------------------------------------------------
+---                       Data                           ---
+------------------------------------------------------------
+
+insert into iet_perfil values (1, 'G_ieventask_administrador', null);
+
+insert into iet_permiso values (1, 'Gestionar perfiles');
+insert into iet_permiso values (2, 'Gestionar eventos');
+insert into iet_permiso values (3, 'Crear y modificar tareas');
+insert into iet_permiso values (4, 'Crear y modificar tareas no asignadas');
+insert into iet_permiso values (5, 'Crear y modificar objetivos');
+insert into iet_permiso values (6, 'Crear y modificar objetivos no asignadas');
+insert into iet_permiso values (7, 'Agregar comentarios');
+insert into iet_permiso values (8, 'Agregar comentarios no asignadas');
+insert into iet_permiso values (9, 'Asignar Tareas');
+insert into iet_permiso values (10, 'Reasignar Tareas');
+insert into iet_permiso values (11, 'Subdividir Tareas');
+
+insert into iet_permiso_perfil values (1,1);
+insert into iet_permiso_perfil values (2,1);
+insert into iet_permiso_perfil values (3,1);
+insert into iet_permiso_perfil values (4,1);
+insert into iet_permiso_perfil values (5,1);
+insert into iet_permiso_perfil values (6,1);
+insert into iet_permiso_perfil values (7,1);
+insert into iet_permiso_perfil values (8,1);
+insert into iet_permiso_perfil values (9,1);
+insert into iet_permiso_perfil values (10,1);
+insert into iet_permiso_perfil values (11,1);
--- trunk/iEvenTaskBackEnd/startup/DropTables.sql
+++ trunk/iEvenTaskBackEnd/startup/DropTables.sql
@@ -1,65 +1,101 @@
+------------------------------------------------------------
+---                  Restrictions                        ---
+------------------------------------------------------------
+
 ALTER TABLE iet_permiso_perfil 
 DROP CONSTRAINT fk_perp_permiso;
+
 ALTER TABLE iet_permiso_perfil 
 DROP CONSTRAINT fk_perp_perfil;
-DROP TABLE iet_permiso_perfil;
-drop table iet_permiso;
-drop table iet_perfil;
 
-ALTER TABLE iet_visualiza 
-DROP CONSTRAINT fk_visualiza_tarea;
+ALTER TABLE iet_permiso_perfil 
+DROP CONSTRAINT pk_perp;
 
-DROP TABLE iet_visualiza;
+ALTER TABLE iet_subtarea 
+DROP CONSTRAINT fk_subtarea_tarea;
+
+ALTER TABLE iet_subtarea 
+DROP CONSTRAINT fk_subtarea_subtarea;
+
+ALTER TABLE iet_subtarea 
+DROP CONSTRAINT pk_subtarea;
 
 ALTER TABLE iet_comentario 
 DROP CONSTRAINT fk_comentario_tarea;
 
-DROP TABLE iet_comentario;
+ALTER TABLE iet_comentario 
+DROP CONSTRAINT pk_comentario;
 
-ALTER TABLE iet_subtarea 
-DROP CONSTRAINT fk_subtarea_tarea;
-ALTER TABLE iet_subtarea 
-DROP CONSTRAINT fk_subtarea_subtarea;
+ALTER TABLE iet_visualiza 
+DROP CONSTRAINT fk_visualiza_tarea;
 
-DROP TABLE iet_subtarea;
-
-DROP TABLE iet_tarea;
+ALTER TABLE iet_visualiza 
+DROP CONSTRAINT pk_visualiza;
 
 ALTER TABLE iet_evento_ldap 
 DROP CONSTRAINT fk_evento_eventoldap;
+
 ALTER TABLE iet_evento_logs 
 DROP CONSTRAINT fk_evento_eventologs;
+
 ALTER TABLE iet_evento_archivos 
 DROP CONSTRAINT fk_evento_eventoarchivos;
-ALTER TABLE iet_evento_servicios
+
+ALTER TABLE iet_evento_servicios 
 DROP CONSTRAINT fk_evento_eventoservicios;
-ALTER TABLE iet_relacion
+
+ALTER TABLE iet_relacion 
 DROP CONSTRAINT fk_relacion_evento;
 
-
-DROP TABLE iet_evento_ldap;
-DROP TABLE iet_evento_logs;
-DROP TABLE iet_evento_archivos;
-DROP TABLE iet_evento_servicios;
-DROP TABLE iet_evento;
-
-ALTER TABLE iet_relacion_nueva_tarea
+ALTER TABLE iet_relacion_nueva_tarea 
 DROP CONSTRAINT fk_relacion_nueva_tarea_relacion;
-ALTER TABLE iet_relacion_modifica_estado
+
+ALTER TABLE iet_relacion_modifica_estado 
 DROP CONSTRAINT fk_relacion_modifica_estado_relacion;
 
-DROP TABLE iet_relacion_nueva_tarea;
-DROP TABLE iet_relacion_modifica_estado;
+ALTER TABLE iet_visible 
+DROP CONSTRAINT fk_relacion_tarea_visible;
+
+ALTER TABLE iet_obj_visible 
+DROP CONSTRAINT fk_relacion_objetivo_visible;
+
+
+
+------------------------------------------------------------
+---                  DROPS                               ---
+------------------------------------------------------------
+DROP TABLE iet_perfil;
+
+DROP TABLE iet_permiso;
+
+DROP TABLE iet_permiso_perfil;
+
+DROP TABLE iet_tarea;
+
+DROP TABLE iet_subtarea;
+
+DROP TABLE iet_comentario;
+
+DROP TABLE iet_visualiza ;
+
+DROP TABLE iet_evento;
+
+DROP TABLE iet_evento_ldap;
+
+DROP TABLE iet_evento_logs;
+
+DROP TABLE iet_evento_archivos;
+
+DROP TABLE iet_evento_servicios;
+
 DROP TABLE iet_relacion;
+
+DROP TABLE iet_relacion_nueva_tarea;
+
+DROP TABLE iet_relacion_modifica_estado;
 
 DROP TABLE iet_objetivo;
 
-ALTER TABLE iet_visible
-DROP CONSTRAINT fk_relacion_tarea_visible;
-
 DROP TABLE iet_visible;
-
-ALTER TABLE iet_obj_visible
-DROP CONSTRAINT fk_relacion_objetivo_visible;
 
 DROP TABLE iet_obj_visible;
